451a4b02a4a61a0b3c2d4142cb38c3170806db92
Sonali Coelho <thesonalimarie@gmail.com>
2024-01-16 19:34:48 -0800

    Added command sequence to characterize swerve drive in teleop.

--- src/main/java/frc/robot/commands/SysID.java
+++ src/main/java/frc/robot/commands/SysID.java
@@ -11,6 +11,7 @@ import static edu.wpi.first.units.Units.Volts;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import org.javatuples.LabelValue;
 
@@ -23,6 +24,7 @@ import edu.wpi.first.units.Velocity;
 import edu.wpi.first.units.Voltage;
 import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.subsystems.Subsystems;
@@ -33,6 +35,7 @@ public class SysID {
     private static final MutableMeasure<Voltage> appliedVoltage = mutable(Volts.of(0));
     private static final MutableMeasure<Distance> distance = mutable(Meters.of(0));
     private static final MutableMeasure<Velocity<Distance>> velocity = mutable(MetersPerSecond.of(0));
+    private static int currentTest = 0;
 
     /**
      * Returns the commands to characterize the swerve drive.
@@ -40,32 +43,64 @@ public class SysID {
      * @param subsystems The subsystems container.
      * @return The commands to characterize the swerve drive.
      */
-    @AutonomousCommandGenerator
-    public static Collection<LabelValue<String, Command>> getSwerveDriveCharacterizationCommands(Subsystems subsystems) {
+    public static Collection<LabelValue<String, Command>> getSwerveDriveCharacterizationCommands(
+            Subsystems subsystems) {
         SysIdRoutine.Config routineConfig = new SysIdRoutine.Config();
         SysIdRoutine.Mechanism mechanism = new SysIdRoutine.Mechanism(
-            (Measure<Voltage> volts) -> {
-                SwerveModuleVoltages voltages = new SwerveModuleVoltages(volts.in(Volts), 0.0);
-                SwerveModuleVoltages[] moduleVoltages = new SwerveModuleVoltages[] {
-                    voltages, voltages, voltages, voltages
-                };
-                subsystems.drivetrain.setModuleVoltages(moduleVoltages);
-                appliedVoltage.mut_replace(volts);
-            }, 
-            (SysIdRoutineLog log) -> {
-                log.motor("Drive")
-                    .voltage(appliedVoltage)
-                    .linearPosition(distance.mut_replace(subsystems.drivetrain.getPosition().getX(), Meters))
-                    .linearVelocity(velocity.mut_replace(subsystems.drivetrain.getChassisSpeeds().vxMetersPerSecond, MetersPerSecond));
+                (Measure<Voltage> volts) -> {
+                    SwerveModuleVoltages voltages = new SwerveModuleVoltages(volts.in(Volts), 0.0);
+                    SwerveModuleVoltages[] moduleVoltages = new SwerveModuleVoltages[] {
+                            voltages, voltages, voltages, voltages
+                    };
+                    subsystems.drivetrain.setModuleVoltages(moduleVoltages);
+                    appliedVoltage.mut_replace(volts);
+                },
+                (SysIdRoutineLog log) -> {
+                    log.motor("Drive")
+                            .voltage(appliedVoltage)
+                            .linearPosition(distance.mut_replace(subsystems.drivetrain.getPosition().getX(), Meters))
+                            .linearVelocity(velocity.mut_replace(
+                                    subsystems.drivetrain.getChassisSpeeds().vxMetersPerSecond, MetersPerSecond));
 
-            }, 
-            subsystems.drivetrain);
+                },
+                subsystems.drivetrain);
         SysIdRoutine routine = new SysIdRoutine(routineConfig, mechanism);
         return List.of(
-            new LabelValue<String, Command>("Swerve Drive Quasistatic Forward", routine.quasistatic(Direction.kForward)), 
-            new LabelValue<String, Command>("Swerve Drive Quasistatic Reverse", routine.quasistatic(Direction.kReverse)),
-            new LabelValue<String, Command>("Swerve Drive Dynamic Forward", routine.dynamic(Direction.kForward)),
-            new LabelValue<String, Command>("Swerve Drive Dynamic Reverse", routine.dynamic(Direction.kReverse))
-        );
+                new LabelValue<String, Command>("Swerve Drive Quasistatic Forward",
+                        routine.quasistatic(Direction.kForward)),
+                new LabelValue<String, Command>("Swerve Drive Quasistatic Reverse",
+                        routine.quasistatic(Direction.kReverse)),
+                new LabelValue<String, Command>("Swerve Drive Dynamic Forward", routine.dynamic(Direction.kForward)),
+                new LabelValue<String, Command>("Swerve Drive Dynamic Reverse", routine.dynamic(Direction.kReverse)));
     }
+
+    /**
+     * Returns the command sequence used to characterize the swerve drive.
+     * 
+     * @param subsystems The subsystems container.
+     * @return The command sequence.
+     */
+    public static Command getSwerveDriveCharacterizationSequence(Subsystems subsystems) {
+        Command[] testCommands = getSwerveDriveCharacterizationCommands(subsystems)
+                .stream()
+                .map(lv -> lv.getValue())
+                .toArray(Command[]::new);
+
+        Command testSequence = Commands.select(
+                Map.of(
+                        0, testCommands[0],
+                        1, testCommands[1],
+                        2, testCommands[2],
+                        3, testCommands[3]),
+                () -> {
+                    int testIndex = currentTest++;
+                    if (currentTest >= 4) {
+                        currentTest = 0;
+                    }
+                    return testIndex;
+                });
+        return testSequence;
+
+    }
+
 }
