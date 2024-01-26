51a417e9e9af039abd7943ba71a7947c809a3658
Harry Ouyang <oyjunhan@hotmail.com>
2024-01-25 21:33:37 -0800

    Implement a command that auto-pathfinds to speaker

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.RobotConstants.OperatorConstants;
 import frc.robot.commands.DriveUsingController;
+import frc.robot.commands.Pathfinding;
 import frc.robot.commands.SysID;
 import frc.robot.subsystems.Subsystems;
 
@@ -74,6 +75,7 @@ public class RobotContainer {
     m_driverController.start().onTrue(Commands.runOnce(() -> m_subsystems.drivetrain.resetOrientation(), m_subsystems.drivetrain));
     m_driverController.back().whileTrue(SysID.getSwerveDriveCharacterizationSequence(m_subsystems));
     m_driverController.leftBumper().whileTrue(SysID.getSwerveSteeringCharacterizationSequence(m_subsystems));
+    m_driverController.a().onTrue(Pathfinding.pathFindToSpeakerFront(m_subsystems));
   }
 
   /**
