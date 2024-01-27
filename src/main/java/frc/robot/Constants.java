859c7c55ebe5e7d6eebac1126d0ba0eaa942fa45
Space9999 <sboddams@gmail.com>
2024-01-27 10:04:37 -0800

    Adds extra code to resolve merge conflict

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
@@ -37,13 +40,37 @@ public final class Constants {
      * Field of View of Camera in degrees.
      */
     public static final double CAMERA_FOV = 70;
-    
+
     public static final Transform3d APRILTAG_CAMERA_TO_ROBOT = new Transform3d();
     
   /**
    * Defines operator (i.e. driver and manipulator) constants.
    */
   public static class OperatorConstants {
+    
+    /** 3d transforms that moves the camera to the center of the robot. */
+    public static final Transform3d APRILTAG_CAMERA_TO_ROBOT = new Transform3d(
+      new Translation3d(Units.inchesToMeters(-3.5), Units.inchesToMeters(-15.25), Units.inchesToMeters(-23)),
+      new Rotation3d(0, Math.toRadians(13.3), Math.toRadians(180))
+    );
+
+    /** Distance robot should be from speaker to score. */
+    public static final double SCORING_DISTANCE_FROM_SPEAKER = Units.inchesToMeters(100);
+
+    /** Distance robot should be from amp to score. */
+    public static final double SCORING_DISTANCE_FROM_AMP = Units.inchesToMeters(20);
+
+    /**
+     * CAN Ids
+     */
+    public static class CAN {
+      public static class SparkMax {
+        public static final int SHOOTER_PORT = 16;
+        public static final int INTAKE_PORT = 17; // TODO assign actual port
+        public static final int ARM_LEFT_PORT = 18; // TODO assign actual port
+        public static final int ARM_RIGHT_PORT = 19; // TODO assign actual port
+      }
+    }
 
     /**
      * Defines the port numbers of the Xbox controllers.
