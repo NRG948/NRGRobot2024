b87aadc6b33dfd7896c15918ca098ab8c061dc3c
Reshma Winter <reshmawinter@gmail.com>
2024-02-03 15:19:12 -0800

    Added additional LED effects and gamma corrections. Co-Authored-By: nikitawarrier <nikitawarrier@gmail.com> Co-Authored-By: AudreyZ19 <auddog19@gmail.com> Co-Authored-By: griffey07 <griffeyy07@gmail.com>

---
+++ src/main/java/frc/robot/commands/FlashingLEDs.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.AddressableLEDSubsystem;
+
+public class FlashingLEDs extends Command {
+  private final Color8Bit color1;
+  private final Color8Bit color2;
+  private final AddressableLEDSubsystem led;
+  private final Timer timer = new Timer();
+  private final double blinkingSpeed;
+
+  private Color8Bit color;
+
+  /** Creates a new FlashingLEDs. */
+  public FlashingLEDs(Color8Bit color1, Color8Bit color2, AddressableLEDSubsystem led, double blinkingSpeed) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.color1 = color1;
+    this.color2 = color2;
+    this.led = led;
+    this.blinkingSpeed = blinkingSpeed;
+    addRequirements(led);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    led.fillAndCommitColor(color1);
+    timer.start();
+    color = color1;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (timer.advanceIfElapsed(blinkingSpeed)) {
+      if (color.equals(color1)) {
+        color = color2;
+      } else {
+        color = color1;
+      }
+      led.fillAndCommitColor(color);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
