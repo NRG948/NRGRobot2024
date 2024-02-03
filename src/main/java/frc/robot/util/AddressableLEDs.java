1e57f259b1072f04df512d345797aba72d929fb4
Reshma Winter <reshmawinter@gmail.com>
2024-02-03 11:28:03 -0800

    Add support for multiple LED segments. Co-Authored-By: griffey07 <griffeyy07@gmail.com> Co-Authored-By: nikitawarrier <nikitawarrier@gmail.com> Co-Authored-By: AudreyZ19 <auddog19@gmail.com>

--- src/main/java/frc/robot/util/AddressableLEDs.java
+++ src/main/java/frc/robot/util/AddressableLEDs.java
@@ -7,41 +7,63 @@ package frc.robot.util;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.util.Color8Bit;
+import frc.robot.Constants.RobotConstants;
 
 /** Add your docs here. */
 public class AddressableLEDs {
-    private AddressableLED leds;
-    private final AddressableLEDBuffer ledBuffer;
+    private static final AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(RobotConstants.LED_COUNT);
+    private static final AddressableLED leds = createAddressableLED(ledBuffer);
+    private final int firstLED;
+    private final int numberOfLEDs;
 
-    public AddressableLEDs(int port, int numberOfLEDs) {
-        leds = new AddressableLED(port);
-        leds.setLength(numberOfLEDs);
-        ledBuffer = new AddressableLEDBuffer(numberOfLEDs);
-        leds.setData(ledBuffer);
+    /**
+     * Creates and initializes an addressable LED.
+     * 
+     * @param buffer The addressable LED buffer.
+     */
+    private static AddressableLED createAddressableLED(AddressableLEDBuffer buffer) { 
+        AddressableLED led = new AddressableLED(RobotConstants.PWMPort.LED);
+        led.setLength(RobotConstants.LED_COUNT);
+        led.setData(buffer);
+        led.start();
+        return led;
     }
 
-    public AddressableLED getLED() {
-        return leds;
+    /**
+     * Constructs an instance of this class.
+     * 
+     * @param firstLED The index of the first LED.
+     * @param numberOfLEDs The number of LEDs.
+     */
+    public AddressableLEDs(int firstLED, int numberOfLEDs) {
+        this.firstLED = firstLED;
+        this.numberOfLEDs = numberOfLEDs;
     }
 
-    public void start() {
-        leds.start();
-    }
-
-    public void stop() {
-        leds.stop();
-    }
-
-    public void setColor(Color8Bit color) {
-        for (var i = 0; i < ledBuffer.getLength(); i++) {
-            ledBuffer.setRGB(i, color.red, color.green, color.blue);
+    /**
+     * Fills the LED segment with a specific color.
+     * 
+     * @param color The color it fills the LEDs.
+     */
+    public void fill(Color8Bit color) {
+        for (var i = 0; i < numberOfLEDs; i++) {
+            ledBuffer.setRGB(i + firstLED, color.red, color.green, color.blue);
         }
     }
 
+    /**
+     * Sets the color at the given LED index.
+     * 
+     * @param color The color it fills the LEDs.
+     * @param index Where the LED is set.
+     */
     public void setColor(Color8Bit color, int index) {
-        ledBuffer.setRGB(index, color.red, color.green, color.blue);
+        ledBuffer.setRGB(index + firstLED, color.red, color.green, color.blue);
     }
 
+    /**
+     * Sends the LED data to the LED string.
+     */
     public void commitColor() {
         leds.setData(ledBuffer);
     }
