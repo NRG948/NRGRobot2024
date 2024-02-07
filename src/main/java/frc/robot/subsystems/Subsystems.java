bdcae15894ed8e1d8907f74f994454e479ab9435
Harry Ouyang <oyjunhan@hotmail.com>
2024-02-06 20:57:59 -0800

    Add absolute encoder and manual arm control.

--- src/main/java/frc/robot/subsystems/Subsystems.java
+++ src/main/java/frc/robot/subsystems/Subsystems.java
@@ -3,28 +3,31 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
+
 import com.nrg948.preferences.RobotPreferencesValue;
 import com.nrg948.preferences.RobotPreferences;
 
 /** Add your docs here. */
 public class Subsystems {
   @RobotPreferencesValue
-  public static final RobotPreferences.BooleanValue ENABLE_POSE_ESTIMATION = new RobotPreferences.BooleanValue("AprilTag", "Enable Pose Estimation", true);
-
+  public static final RobotPreferences.BooleanValue ENABLE_POSE_ESTIMATION = new RobotPreferences.BooleanValue(
+      "AprilTag", "Enable Pose Estimation", true);
+      
   public SwerveSubsystem drivetrain = new SwerveSubsystem();
   public AprilTagSubsystem aprilTag = new AprilTagSubsystem();
   public NoteVisionSubsystem noteVision = new NoteVisionSubsystem();
   public IndexerSubsystem indexerSubsystem = new IndexerSubsystem();
   public AddressableLEDSubsystem addressableLEDSubsystem = new AddressableLEDSubsystem();
+  public ArmSubsystem armSubsystem = new ArmSubsystem();
   // public IntakeSubsystem intake = new IntakeSubsystem();
 
   public void periodic() {
-    if (ENABLE_POSE_ESTIMATION.getValue()){
+    if (ENABLE_POSE_ESTIMATION.getValue()) {
       var visionEst = aprilTag.getEstimateGlobalPose();
       visionEst.ifPresent((est) -> {
         var estPose = est.estimatedPose.toPose2d();
         var estStdDevs = aprilTag.getEstimationStdDevs(estPose);
-  
+
         drivetrain.addVisionMeasurement(estPose, est.timestampSeconds, estStdDevs);
       });
     }
