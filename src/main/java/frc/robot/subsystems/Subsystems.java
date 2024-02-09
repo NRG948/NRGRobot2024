21d51deba47662efd3c5d6539f530eed8cbbc50b
Nilesh <45780409+thaknil24@users.noreply.github.com>
2024-02-08 19:10:28 -0800

    Add enable pose esimation preference
    Co-Authored-By: ZZeeZZ1127 <zeen1127.zz@gmail.com>

--- src/main/java/frc/robot/subsystems/Subsystems.java
+++ src/main/java/frc/robot/subsystems/Subsystems.java
@@ -3,9 +3,14 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
+import com.nrg948.preferences.RobotPreferencesValue;
+import com.nrg948.preferences.RobotPreferences;
 
 /** Add your docs here. */
 public class Subsystems {
+  @RobotPreferencesValue
+  public static final RobotPreferences.BooleanValue ENABLE_POSE_ESTIMATION = new RobotPreferences.BooleanValue("AprilTag", "Enable Pose Estimation", true);
+
   public SwerveSubsystem drivetrain = new SwerveSubsystem();
   public AprilTagSubsystem aprilTag = new AprilTagSubsystem();
   public NoteVisionSubsystem noteVision = new NoteVisionSubsystem();
@@ -14,12 +19,14 @@ public class Subsystems {
   // public IntakeSubsystem intake = new IntakeSubsystem();
 
   public void periodic() {
-    var visionEst = aprilTag.getEstimateGlobalPose();
-    visionEst.ifPresent((est) -> {
-      var estPose = est.estimatedPose.toPose2d();
-      var estStdDevs = aprilTag.getEstimationStdDevs(estPose);
-
-      drivetrain.addVisionMeasurement(estPose, est.timestampSeconds, estStdDevs);
-    });
+    if (ENABLE_POSE_ESTIMATION.getValue()){
+      var visionEst = aprilTag.getEstimateGlobalPose();
+      visionEst.ifPresent((est) -> {
+        var estPose = est.estimatedPose.toPose2d();
+        var estStdDevs = aprilTag.getEstimationStdDevs(estPose);
+  
+        drivetrain.addVisionMeasurement(estPose, est.timestampSeconds, estStdDevs);
+      });
+    }
   }
 }
