c967348787d601b2e45375722d815d0aef95672b
Nilesh <45780409+thaknil24@users.noreply.github.com>
2024-02-11 15:15:29 -0800

    Implemented alliance based teleop driving relative to blue origin Co-Authored-By: pranavdasan <pranavdasan8@gmail.com>

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -4,18 +4,26 @@
 
 package frc.robot.commands;
 
+import java.util.Map;
+import java.util.Optional;
+
 import com.nrg948.preferences.RobotPreferences;
 import com.nrg948.preferences.RobotPreferencesValue;
 
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Constants.RobotConstants;
+import frc.robot.drive.SwerveDrive;
 import frc.robot.subsystems.AprilTagSubsystem;
 import frc.robot.subsystems.Subsystems;
+import frc.robot.subsystems.SwerveSubsystem;
 
 public final class DriveCommands {
   @RobotPreferencesValue
@@ -59,5 +67,26 @@ public final class DriveCommands {
 
     return Commands.sequence(
         new DriveStraight(drivetrain, goalPose, drivetrain.getMaxSpeed() * 0.5));
+  }
+
+  /**
+   * Returns a command that resets the drivetrain orientation for teleop driving based on the current alliance.
+   * 
+   * @param subsystems The subsystems container.
+   * @return A command to reset the drivetrain orientation.
+   */
+  public static Command resetOrientation(Subsystems subsystems) {
+    SwerveSubsystem drivetrain = subsystems.drivetrain;
+
+    return Commands.select(
+      Map.of(
+        Alliance.Blue, Commands.runOnce(() -> drivetrain.resetOrientation(new Rotation2d()), drivetrain), 
+        Alliance.Red, Commands.runOnce(() -> drivetrain.resetOrientation(Rotation2d.fromDegrees(180)), drivetrain)
+      ), 
+      () -> {
+        Optional<Alliance> alliance = DriverStation.getAlliance();
+        return alliance.orElse(Alliance.Blue);
+      });
+
   }
 }
