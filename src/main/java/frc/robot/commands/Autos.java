28df5f2035b18a8837f16590a7d32d1e05a3334f
griffey07 <griffeyy07@gmail.com>
2024-02-24 12:01:55 -0800

    Complete Source Sweep Auto.

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -7,12 +7,17 @@
 package frc.robot.commands;
 
 import com.nrg948.autonomous.AutonomousCommandGenerator;
+import com.nrg948.preferences.RobotPreferences;
+import com.nrg948.preferences.RobotPreferencesValue;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.IndexerSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.Subsystems;
 import frc.robot.subsystems.SwerveSubsystem;
 import java.io.File;
@@ -24,6 +29,65 @@ import java.util.Set;
 import org.javatuples.LabelValue;
 
 public final class Autos {
+  @RobotPreferencesValue
+  public static RobotPreferences.DoubleValue SOURCE_FARSHOT_RPM =
+      new RobotPreferences.DoubleValue(
+          "Autonomous", "Source Far Shot RPM", 4500); // TODO: RPM needs to be tested.
+
+  @RobotPreferencesValue
+  public static RobotPreferences.DoubleValue AMP_FARSHOT_RPM =
+      new RobotPreferences.DoubleValue("Autonomous", "Amp Far Shot RPM", 4500);
+
+  @RobotPreferencesValue
+  public static RobotPreferences.DoubleValue SPIKE_SHOT_RPM =
+      new RobotPreferences.DoubleValue(
+          "Autonomous", "Spike Shot RPM", 2000); // TODO: RPM needs to be tested.
+
+  @RobotPreferencesValue
+  public static RobotPreferences.DoubleValue MID_SPIKE_SHOT_RPM =
+      new RobotPreferences.DoubleValue(
+          "Autonomous", "Mid Spike Shot RPM", 2000); // TODO: RPM needs to be tested.
+
+  @RobotPreferencesValue
+  public static RobotPreferences.DoubleValue SOURCE_FARSHOT_ANGLE =
+      new RobotPreferences.DoubleValue(
+          "Autonomous", "Source Far Shot Angle", 10); // YODO: Angle needs to be tested.
+
+  @RobotPreferencesValue
+  public static RobotPreferences.DoubleValue AMP_FARSHOT_ANGLE =
+      new RobotPreferences.DoubleValue(
+          "Autonomous", "Amp Far Shot Angle", 22); // TODO: Angle needs to be tested.
+
+  @RobotPreferencesValue
+  public static RobotPreferences.DoubleValue SPIKE_SHOT_ANGLE =
+      new RobotPreferences.DoubleValue(
+          "Autonomous", "Spike Shot Angle", 12); // TODO: Angle needs to be tested.
+
+  @RobotPreferencesValue
+  public static RobotPreferences.DoubleValue MID_SPIKE_SHOT_ANGLE =
+      new RobotPreferences.DoubleValue(
+          "Autonomous", "Mid Spike Shot Angle", 10); // TODO: Angle needs to be tested.
+
+  @RobotPreferencesValue
+  public static RobotPreferences.DoubleValue SHOOT_SOURCE_FARSHOT =
+      new RobotPreferences.DoubleValue(
+          "Autonomous", "Source Far Shot Angle", 10); // YODO: Angle needs to be tested.
+
+  @RobotPreferencesValue
+  public static RobotPreferences.DoubleValue SHOOT_AMP_FARSHOT =
+      new RobotPreferences.DoubleValue(
+          "Autonomous", "Amp Far Shot Angle", 22); // TODO: Angle needs to be tested.
+
+  @RobotPreferencesValue
+  public static RobotPreferences.DoubleValue SHOOT_SPIKE_SHOT =
+      new RobotPreferences.DoubleValue(
+          "Autonomous", "Spike Shot Angle", 12); // TODO: Angle needs to be tested.
+
+  @RobotPreferencesValue
+  public static RobotPreferences.DoubleValue SHOOT_MID_SPIKE_SHOT =
+      new RobotPreferences.DoubleValue(
+          "Autonomous", "Mid Spike Shot Angle", 10); // TODO: Angle needs to be tested.
+
   /**
    * Returns the collection of PathPlanner auto commands.
    *
@@ -52,41 +116,59 @@ public final class Autos {
     SwerveSubsystem driveTrain = subsystems.drivetrain;
     Pose2d startPose = PathPlannerAuto.getStaringPoseFromAutoFile(name);
 
-    // Map<String, Command> map = getPathplannerEventMap(subsystems, name);
-    // map.get(name);
     NamedCommands.registerCommands(getPathplannerEventMap(subsystems, name));
-    // Command test = get
 
     return Commands.sequence(
         Commands.runOnce(() -> driveTrain.resetPosition(startPose), driveTrain),
         Commands.defer(() -> new PathPlannerAuto(name), Set.of(driveTrain)));
-    // Commands.runOnce(() -> )
   }
-
-  // public static void registerCommands(Map<String, Command> commands){
-  // NamedCommands.re;
-  // }
 
   private static Map<String, Command> getPathplannerEventMap(
       Subsystems subsystems, String pathGroupName) {
 
     Map<String, Command> eventMaps =
         new HashMap<String, Command>(); // TODO: Replace placeholder parameters
-    eventMaps.put("SetShooterRPMSpike", ShooterCommands.setAndWaitForRPM(subsystems, 2000));
-    eventMaps.put("SetShooterRPMAmpFarShot", ShooterCommands.setAndWaitForRPM(subsystems, 5000));
-    eventMaps.put("SetShooterRPMSourceFarShot", ShooterCommands.setAndWaitForRPM(subsystems, 5000));
-    eventMaps.put("SetArmAngleSpike", ArmCommands.seekToAngle(subsystems, Math.toRadians(15)));
-    eventMaps.put("SetArmAngleAmpFarShot", ArmCommands.seekToAngle(subsystems, Math.toRadians(10)));
-    eventMaps.put(
-        "SetArmAngleSourceFarShot", ArmCommands.seekToAngle(subsystems, Math.toRadians(10)));
+    eventMaps.put("SetShooterRPMAmpFarShot", setShooterRPM(subsystems, AMP_FARSHOT_RPM));
+    eventMaps.put("SetShooterRPMSourceFarShot", setShooterRPM(subsystems, SOURCE_FARSHOT_RPM));
+    eventMaps.put("SetShooterRPMSpikeShot", setShooterRPM(subsystems, SPIKE_SHOT_RPM));
+    eventMaps.put("SetShooterRPMMidSpikeShot", setShooterRPM(subsystems, MID_SPIKE_SHOT_RPM));
+
+    eventMaps.put("SetArmAngleSpikeShot", setArmAngle(subsystems, SPIKE_SHOT_ANGLE));
+    eventMaps.put("SetArmAngleAmpFarShot", setArmAngle(subsystems, AMP_FARSHOT_ANGLE));
+    eventMaps.put("SetArmAngleSourceFarShot", setArmAngle(subsystems, SOURCE_FARSHOT_ANGLE));
+    eventMaps.put("SetArmAngleMidSpikeShot", setArmAngle(subsystems, MID_SPIKE_SHOT_ANGLE));
+
+    eventMaps.put("ShootSourceFarShot", shoot(subsystems, SHOOT_SOURCE_FARSHOT));
+    eventMaps.put("ShootAmpFarShot", shoot(subsystems, SHOOT_AMP_FARSHOT));
+    eventMaps.put("ShootSpikeShot", shoot(subsystems, SHOOT_SPIKE_SHOT));
+    eventMaps.put("ShootMidSPikeShot", shoot(subsystems, SHOOT_MID_SPIKE_SHOT));
+
     eventMaps.put("FeedIndexerFullPower", Commands.runOnce(() -> subsystems.indexer.feed()));
     eventMaps.put("StowArm", ArmCommands.stow(subsystems));
-    eventMaps.put("Intake", NoteCommands.intake(subsystems)); // will run our intake sequence
+    eventMaps.put("Intake", NoteCommands.intake(subsystems));
+    eventMaps.put("IntakeUntilNoteDetected", NoteCommands.intakeUntilNoteDetected(subsystems));
 
     return eventMaps;
   }
 
-  public static void NamedCommands() {}
+  private static Command setShooterRPM(Subsystems subsystems, RobotPreferences.DoubleValue rpm) {
+    ShooterSubsystem shooter = subsystems.shooter;
+    return Commands.defer(
+        () -> ShooterCommands.setRPM(subsystems, rpm.getValue()), Set.of(shooter));
+  }
+
+  private static Command setArmAngle(Subsystems subsystems, RobotPreferences.DoubleValue angle) {
+    ArmSubsystem arm = subsystems.arm;
+    return Commands.defer(
+        () -> ArmCommands.seekToAngle(subsystems, Math.toRadians(angle.getValue())), Set.of(arm));
+  }
+
+  public static Command shoot(Subsystems subsystems, RobotPreferences.DoubleValue rpm) {
+    ShooterSubsystem shooter = subsystems.shooter;
+    IndexerSubsystem indexer = subsystems.indexer;
+    return Commands.defer(
+        () -> NoteCommands.shoot(subsystems, rpm.getValue()), Set.of(shooter, indexer));
+  }
 
   private Autos() {
     throw new UnsupportedOperationException("This is a utility class!");
