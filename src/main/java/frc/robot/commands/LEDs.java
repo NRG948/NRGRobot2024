2d6b6b618ce84b9c783b1502bb471d7544df2f97
Harry Ouyang <oyjunhan@hotmail.com>
2024-03-07 20:51:20 -0800

    Add LED flash to successful intake and shooter spinup

--- src/main/java/frc/robot/commands/LEDs.java
+++ src/main/java/frc/robot/commands/LEDs.java
@@ -9,11 +9,21 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants.ColorConstants;
 import frc.robot.subsystems.LEDSubsystem;
 
 /** Add your docs here. */
 public class LEDs {
   public static Command fillColor(LEDSubsystem ledSubsystem, Color8Bit color) {
     return Commands.runOnce(() -> ledSubsystem.fillAndCommitColor(color), ledSubsystem);
+  }
+
+  public static Command flashColor(LEDSubsystem ledSubsystem, Color8Bit color) {
+    return Commands.repeatingSequence(
+            Commands.runOnce(() -> ledSubsystem.fillAndCommitColor(color)),
+            Commands.waitSeconds(0.1),
+            Commands.runOnce(() -> ledSubsystem.fillAndCommitColor(ColorConstants.BLACK)),
+            Commands.waitSeconds(0.1))
+        .withTimeout(1);
   }
 }
