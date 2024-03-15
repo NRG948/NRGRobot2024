e0b5a235723901a7163e4ea9e826cc1a10d651c3
Reshma Winter <reshmawinter@gmail.com>
2024-03-14 19:28:30 -0700

    Created and implemented manualClimbChain and manualClimbDownChain climber commands.
    Co-Authored-By: pranavdasan <pranavdasan8@gmail.com>

---
+++ src/main/java/frc/robot/commands/ClimberCommands.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.Optional;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.ClimberSubsystem;
+import frc.robot.subsystems.Subsystems;
+
+/** Commands to wind and undwind the climber subsystem on the chain. */
+public class ClimberCommands {
+
+    /**
+     * Returns a command to manually wind the climber. 
+     * @param subsystems The subsystems container.
+     * @return A command to manually wind the climber.
+     */
+    public static Command manualClimbChain(Subsystems subsystems) {
+        Optional<ClimberSubsystem> climber = subsystems.climber;
+
+        if (climber.isEmpty()) {
+            return Commands.none();
+        }
+        
+        return Commands.run(() -> climber.get().wind(), climber.get()).finallyDo(() -> climber.get().stopMotors());
+    }
+
+    /**
+     * Returns a command to manually unwind the climber.
+     * @param subsystems The subsystems container.
+     * @return A command to manually unwind the climber.
+     */
+    public static Command manualClimbDownChain(Subsystems subsystems) {
+        Optional<ClimberSubsystem> climber = subsystems.climber;
+
+        if (climber.isEmpty()) {
+            return Commands.none();
+        }
+        
+        return Commands.run(() -> climber.get().unwind(), climber.get()).finallyDo(() -> climber.get().stopMotors());
+    }
+}
