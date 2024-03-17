e9907c43096011d1ffb3277c44a4617a50734cab
Pranav Dasan <pranavdasan8@gmail.com>
2024-03-17 15:30:59 -0700

    Clean-up arm and indexer parameters.

--- src/main/java/frc/robot/parameters/IndexerParameters.java
+++ src/main/java/frc/robot/parameters/IndexerParameters.java
@@ -10,40 +10,45 @@ import static frc.robot.parameters.MotorParameters.NeoV1_1;
 
 /** Add your docs here. */
 public enum IndexerParameters {
-    PracticeBase2024(3 * 26 / 15, 0.033, 0.5, NeoV1_1, true),
-    CompetitionBase2024(3 * 26 / 15, 0.033, 0.5, NeoV1_1, false); //TODO: double check values
+  PracticeBase2024(3 * 26 / 15, 0.033, 0.5, NeoV1_1, true),
+  CompetitionBase2024(3 * 26 / 15, 0.033, 0.5, NeoV1_1, false);
 
-    private final double gearRatio;
-    private final double diameter;
-    private final double mass;
-    private final MotorParameters motorParameters;
-    private final boolean isInverted;
+  private final double gearRatio;
+  private final double diameter;
+  private final double mass;
+  private final MotorParameters motorParameters;
+  private final boolean isInverted;
 
-    private IndexerParameters(double gearRatio, double diameter, double mass, MotorParameters motorParameters, boolean isInverted) {
-        this.gearRatio = gearRatio;
-        this.diameter = diameter;
-        this.mass = mass;
-        this.motorParameters = motorParameters;
-        this.isInverted = isInverted;
-    }
+  private IndexerParameters(
+      double gearRatio,
+      double diameter,
+      double mass,
+      MotorParameters motorParameters,
+      boolean isInverted) {
+    this.gearRatio = gearRatio;
+    this.diameter = diameter;
+    this.mass = mass;
+    this.motorParameters = motorParameters;
+    this.isInverted = isInverted;
+  }
 
-    public double getGearRatio() {
-        return gearRatio;
-    }
+  public double getGearRatio() {
+    return gearRatio;
+  }
 
-    public double getDiameter() {
-        return diameter;
-    }
+  public double getDiameter() {
+    return diameter;
+  }
 
-    public double getMass() {
-        return mass;
-    }
+  public double getMass() {
+    return mass;
+  }
 
-    public boolean getInverted() {
-        return isInverted;
-    }
+  public boolean getInverted() {
+    return isInverted;
+  }
 
-    public MotorParameters getMotorParameters() {
-        return motorParameters;
-    }
+  public MotorParameters getMotorParameters() {
+    return motorParameters;
+  }
 }
