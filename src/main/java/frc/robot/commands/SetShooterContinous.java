42099cfc2aedbd0784aeb9bf8efede38655cf9eb
NRGProgrammer <programming@nrg948.com>
2024-03-20 14:53:26 -0700

    mid stemnasium auto changes

--- src/main/java/frc/robot/commands/SetShooterContinous.java
+++ src/main/java/frc/robot/commands/SetShooterContinous.java
@@ -29,8 +29,10 @@ public class SetShooterContinous extends Command {
   private final ShooterParams[] shooterParams = { // TODO: finish developing interpolation table
     // new ShooterParams(1.0, -11, 2200),
     new ShooterParams(2.0, 2.5, 3100),
+    new ShooterParams(2.5, 7, 3300),
     new ShooterParams(3.0, 10.5, 3500),
     new ShooterParams(4.0, 16, 3900),
+    new ShooterParams(5.0, 20, 4200),
     // new ShooterParams(5.0, 24.5, 4700)
   };
 
