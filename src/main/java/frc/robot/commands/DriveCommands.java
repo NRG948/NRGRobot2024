e63102adf6648c0b5b0aab12f54ecb2d4a164840
NRGProgrammer <programming@nrg948.com>
2024-03-21 16:35:09 -0700

    Restoring the non-vision orient to speaker command. Works if the robot heading is preset.

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -13,6 +13,7 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
@@ -131,19 +132,18 @@ public final class DriveCommands {
                   } else {
                     // The speaker center AprilTag is not visible, so orient the robot to the
                     // absolute location relative to the current estimated robot pose.
-                    // Translation2d aprilTagLocation =
-                    //     subsystems
-                    //         .aprilTag
-                    //         .get()
-                    //         .getSpeakerCenterAprilTagPose()
-                    //         .toPose2d()
-                    //         .getTranslation();
-                    // return Optional.of(
-                    //     aprilTagLocation
-                    //         .minus(drivetrain.getPosition().getTranslation())
-                    //         .getAngle()
-                    //         .rotateBy(ROTATE_180_DEGREES));
-                    return Optional.of(drivetrain.getOrientation());
+                    Translation2d aprilTagLocation =
+                        subsystems
+                            .aprilTag
+                            .get()
+                            .getSpeakerCenterAprilTagPose()
+                            .toPose2d()
+                            .getTranslation();
+                    return Optional.of(
+                        aprilTagLocation
+                            .minus(drivetrain.getPosition().getTranslation())
+                            .getAngle()
+                            .rotateBy(ROTATE_180_DEGREES));
                   }
                 });
           }
